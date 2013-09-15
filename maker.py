6b00124497a1960fd39744006663eed6463f2ebf
Brendan Reddy-Best <b.d.reddybest@gmail.com>
2013-09-14 21:09:26 -0700

    Update maker.py
    cleaner, nicer, smoother

--- maker.py
+++ maker.py
@@ -1,35 +1,32 @@
-from subprocess import call
-includes = ["SoftwareSerial", "Wire", "Servo", "/some/other/path/Library.h"]
-board = "Uno"
-monitor_port = "/dev/tty.usb*"
+import argparse
 
-class Maker():
-  def write_make(self):
-    f = open("Makefile", 'a+')
-    f.writelines("include ./Applications/Arduino.app/Contents/Resources/Java/arduino-mk/Arduino.mk\r\n")
-    f.writelines("BOARD = {0}\r\n".format(board))
-    f.close() 
-    try:
-      f = open("Makefile", "a+")
-      f.writelines("ARDUINO_LIBS = ")
-      f.close()
-      for include in includes:
-        f = open("Makefile", 'a+')
-        f.writelines(" {0}".format(include))
-        f.close()
-    except:
-      print ("failure to write libs")
-    try:
-      f = open("Makefile", "a+")
-      f.writelines("\nMONITOR_PORT= {0}".format(monitor_port))
-      f.close()
-    except:
-      print("failure to write port")
 
-maker = Maker()
-maker.write_make()
+includes = []
+parser = argparse.ArgumentParser(description='generate a Makefile')
+parser.add_argument('--board', dest='board')
+parser.add_argument('--port', dest='monitor_port')
+parser.add_argument('--library', nargs = '*', dest='library')
 
-''' i need a way to set these paths in OSX, i think
+args = parser.parse_args()
+board = args.board
+monitor_port = args.monitor_port
+for include in args.library:
+  includes.append(include)
+
+
+def write_make():
+  f = open("Makefile", 'a+')
+  f.writelines("include ./Applications/Arduino.app/Contents/Resources/Java/arduino-mk/Arduino.mk\r\n")
+  f.writelines("BOARD = {0}\r\n".format(board))
+  f.writelines("ARDUINO_LIBS = ")
+  for include in includes:
+    f.writelines(" {0}".format(include))
+  f.writelines("\r\nMONITOR_PORT = {0}".format(monitor_port))
+  f.close()
+   
+write_make()
+
+''' i need a way to set these paths in bash, i think
 ARDUINO_DIR   = /Applications/Arduino.app/Contents/Resources/Java
 ARDMK_DIR     = /usr/local
 '''
