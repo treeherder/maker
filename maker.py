e4c6e1e3b51e3178c010ba0790d781a373ca3e1b
Brendan Reddy-Best <b.d.reddybest@gmail.com>
2013-09-16 18:41:41 -0700

    Update maker.py

--- maker.py
+++ maker.py
@@ -2,31 +2,33 @@ import argparse
 
 
 includes = []
+# needs platform detection, and version checking 
+ard_dir   = "/Applications/Arduino.app/Contents/Resources/Java" #where to find the stuff it needs
+ard_mk    = "/usr/local"   #arduino-mk directory
+
 parser = argparse.ArgumentParser(description='generate a Makefile')
 parser.add_argument('--board', dest='board')
 parser.add_argument('--port', dest='monitor_port')
-parser.add_argument('--library', nargs = '*', dest='library')
+parser.add_argument('--libs', nargs = '*', dest='libs')
 
 args = parser.parse_args()
 board = args.board
 monitor_port = args.monitor_port
-for include in args.library:
+for include in args.libs:
   includes.append(include)
 
 
 def write_make():
   f = open("Makefile", 'a+')
-  f.writelines("include ./Applications/Arduino.app/Contents/Resources/Java/arduino-mk/Arduino.mk\r\n")
+  f.writelines("ARDUINO_DIR = {0}\r\n".format(ard_dir))
+  f.writelines("ARD_MK =  {0}\r\n".format(ard_mk))
   f.writelines("BOARD = {0}\r\n".format(board))
-  f.writelines("ARDUINO_LIBS = ")
+  f.writelines("ARDUINO_LIBS =")
   for include in includes:
     f.writelines(" {0}".format(include))
   f.writelines("\r\nMONITOR_PORT = {0}".format(monitor_port))
+  f.writelines("include /Applications/Arduino.app/Contents/Resources/Java/arduino-mk/Arduino.mk\r\n")
+
   f.close()
    
 write_make()
-
-''' i need a way to set these paths in osx, i think
-ARDUINO_DIR   = /Applications/Arduino.app/Contents/Resources/Java
-ARDMK_DIR     = /usr/local
-'''
